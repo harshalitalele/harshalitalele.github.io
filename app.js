a3a17e3980216ed4547048eac9f7fcd80eb29849
harshalitalele <harshalitalele@gmail.com>
2018-09-15 18:19:00 +0530

    leftbar updated

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ function setTitles() {
     document.getElementsByTagName("title")[0].innerHTML = details.name;
     var myPic = document.getElementById("picture");
     myPic.setAttribute("src", details.pictureSrc);
+    document.getElementById("picture-tag").innerHTML = details.name;
+    document.getElementById("professional-tag").innerHTML = details.professionTitle;
+    document.getElementById("linkedin-profile").setAttribute("href", details.linkedinLink);
+    document.getElementById("github-profile").setAttribute("href", details.githubLink);
 }
 
 setTitles();
--- resume.js
+++ resume.js
@@ -1,5 +1,8 @@
 var details = {
     name: "Harshali Talele",
-    pictureSrc: "/harshali_talele.jpg",
+    pictureSrc: "harshali_talele.jpg",
+    professionTitle: "Web Developer",
+    linkedinLink: "https://www.linkedin.com/in/harshali-talele-04b8551b",
+    githubLink: "https://github.com/harshalitalele",
     aboutMe: "I am a 26 years old Web Developer in Mumbai, India."
 }
